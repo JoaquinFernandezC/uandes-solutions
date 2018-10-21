87ca3ee4e79748bddb651b69ee1ba8c7267e70ca
Francisco Alvarez <s2globalmail@gmail.com>
2018-10-21 20:03:18 -0300

    Paginacion,orden, cantidad de tareas y filtro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,10 @@ $(document).ready(function(){
         });
     });
 });
+
+$(document).ready(function () {
+  $('#myTable').DataTable({
+    pageLength : 5,
+    lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'Todos']]
+  });
+});
