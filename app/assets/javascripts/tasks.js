8aa69a4fd88e7979a33e4f2365643d69d04df542
e440 <jvejar1@miuandes.cl>
2018-10-22 21:25:10 -0300

    *tasks/index: working with filter

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,4 +1,29 @@
 $(document).on("turbolinks:load", function() {
+    $('#privacy_multiselect').multiselect({
+        enableFiltering: true,
+        buttonWidth: '100%',
+        maxHeight: 400,
+        buttonText: function(options, select) {
+            if (options.length === 0) {
+                return 'Seleccionar ...';
+            }
+            else if (options.length > 2) {
+                return options.length.toString() + ' seleccionados';
+            }
+            else {
+                var labels = [];
+                options.each(function () {
+                    if ($(this).attr('label') !== undefined) {
+                        labels.push($(this).attr('label'));
+                    }
+                    else {
+                        labels.push($(this).html());
+                    }
+                });
+                return labels.join(', ') + '';
+            }
+        }
+    });
   $('#task_name_multiselect').multiselect({
   enableFiltering: true,
   buttonWidth: '100%',
@@ -24,6 +49,32 @@ return labels.join(', ') + '';
 }
 }
 });
+
+    $('#status_multiselect').multiselect({
+        enableFiltering: true,
+        buttonWidth: '100%',
+        maxHeight: 400,
+        buttonText: function(options, select) {
+            if (options.length === 0) {
+                return 'Seleccionar ...';
+            }
+            else if (options.length > 2) {
+                return options.length.toString() + ' seleccionados';
+            }
+            else {
+                var labels = [];
+                options.each(function () {
+                    if ($(this).attr('label') !== undefined) {
+                        labels.push($(this).attr('label'));
+                    }
+                    else {
+                        labels.push($(this).html());
+                    }
+                });
+                return labels.join(', ') + '';
+            }
+        }
+    });
 $('#internal_member_multiselect').multiselect({
   enableFiltering: true,
   buttonWidth: '100%',
