cf1f73c005a2e85dc30d05fcb3b0680f9c757c6e
Alfonso <ajzuniga1@miuandes.cl>
2018-11-18 17:47:39 -0300

    Heroku push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_220232) do
+ActiveRecord::Schema.define(version: 2018_10_23_024810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -393,6 +393,11 @@ ActiveRecord::Schema.define(version: 2018_10_22_220232) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "felonies_rucs", id: false, force: :cascade do |t|
+    t.bigint "ruc_id", null: false
+    t.bigint "felony_id", null: false
   end
 
   create_table "goal_documents", force: :cascade do |t|
