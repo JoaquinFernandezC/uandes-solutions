8f4b4fbd50cb2a7cb0187312738ce3680dff3170
Alfonso <ajzuniga1@miuandes.cl>
2018-11-19 20:05:44 -0300

    oops

--- app/services/adapters/cause_privacy_filter.rb
+++ app/services/adapters/cause_privacy_filter.rb
@@ -8,7 +8,7 @@ module Adapters
           my_cases = current.causes
           all_public = Cause.all().where(privacy:1)
           all_cases = (my_cases + all_public).uniq
-          all_cases = Case.where(id: all_cases.map(&:id))
+          all_cases = Cause.where(id: all_cases.map(&:id))
         end
         all_cases
       end
