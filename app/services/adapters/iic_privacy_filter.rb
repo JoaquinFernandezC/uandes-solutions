68bdfd910125bc3b8d52987f7e2fd90e11722c8a
Alfonso <ajzuniga1@miuandes.cl>
2018-11-19 20:03:43 -0300

    Fixed privacy filters

--- app/services/adapters/iic_privacy_filter.rb
+++ app/services/adapters/iic_privacy_filter.rb
@@ -8,6 +8,7 @@ module Adapters
           my_theme = current.iic_management
           all_public = Iic.all().where(privacy:1)
           all_theme = (my_theme + all_public).uniq
+          all_theme = Iic.where(id: all_theme.map(&:id))
         end
         all_theme
       end
